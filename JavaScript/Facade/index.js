d56779d585f4aa1cde9c8e3c54c19e4dc837deaa
André Rodrigues <andre.rodrigues98@outlook.com>
2024-09-18 23:40:24 -0300

    refactor: add more use examples Facade

--- JavaScript/Facade/index.js
+++ JavaScript/Facade/index.js
@@ -2,8 +2,10 @@ const StoreProductsFacade = require("./StoreProductsFacade");
 
 const storeProduct = StoreProductsFacade();
 console.log(storeProduct.add("ProducT 1"));
-// { name: 'product 1', createdAt: 1726713378856, id: 1 }
+// { name: 'product 1', createdAt: 1726713541251, id: 1 }
 console.log(storeProduct.add("ProDucT 2"));
-// { name: 'product 2', createdAt: 1726713378865, id: 2 }
+// { name: 'product 2', createdAt: 1726713541258, id: 2 }
 console.log(storeProduct.add("PRODUCT 3"));
-// { name: 'product 3', createdAt: 1726713378865, id: 3 }
+// { name: 'product 3', createdAt: 1726713541258, id: 3 }
+console.log(storeProduct.add("PRODUCT 3"));
+// undefined
